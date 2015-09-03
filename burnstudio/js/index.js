e78188956b7506b959def5f9f1e3853176ec677c
rainyjune <rainyjune@live.cn>
2015-09-03 13:36:44 +0800

    Update burnstudio: add slides

---
+++ burnstudio/js/index.js
@@ -0,0 +1,14 @@
+$(function(){
+	$("#slideshow").slidesjs({
+		width: 960,
+		height: 312,
+		navigation: {
+			active: true,
+			effect: 'slide'
+		},
+		pagination: {
+			active: true,
+			effect: 'slide'
+		}
+	});
+});
