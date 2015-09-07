821fb09352438e01e242c3a78137b6104948f890
rainyjune <rainyjune@live.cn>
2015-09-07 15:23:49 +0800

    Use the grunt-contrib-sass plugin and the grunt-contrib-watch plugin

--- burnstudio/Gruntfile.js
+++ burnstudio/Gruntfile.js
@@ -12,9 +12,17 @@ module.exports = function(grunt) {
           'css/style.css': 'scss/style.scss'          
         }
       }
+    },
+    watch: {
+      css: {
+        files: 'scss/*.scss',
+        tasks: ['sass']
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   grunt.registerTask('default', ['sass']);
 };
